b94cf535ae6601d2d9cffd0fc531262040e62416
ssn <shaamnatheshwar@gmail.com>
2022-12-05 12:17:39 +0530

    pucca filtering

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField : '',
     };
   }
 
@@ -20,24 +21,25 @@ class App extends Component {
   }
 
   render() {
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
     return (
       <div className="App">
         <input
           className="search-box"
-          type= 'search'
+          type="search"
           placeholder="search monsters"
           onChange={(event) => {
-            console.log(event);
-
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.includes(event.target.value);
-            });
+            const searchField = event.target.value.toLocaleLowerCase();
+            
             this.setState(() => {
-              return { monsters: filteredMonsters };
+              return { searchField };
             });
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
